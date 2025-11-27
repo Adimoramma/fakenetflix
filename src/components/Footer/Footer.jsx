c8f03f5eee99966629c1091f914f6cd703fac52e
Theresa Dialla <dialatheresa9@gmail.com>
2025-11-27 13:24:47 +0100

    new commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,31 @@ import youtubeIcon from '../../assets/youtube_icon.png'
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <p>This is the footer</p>
+    <div className='footer'>
+        <div className='icons'>
+     <img src={facebookIcon} alt="facebook"  />
+     <img src={twitterIcon} alt="instagram" />
+     <img src={instagramIcon} alt="twitter"  />
+     <img src={youtubeIcon} alt="youtube" />
+       </div>
+     <ul>
+        <li><Link className='footer-link' to="/audio-description">Audio Description</Link></li>
+        <li><Link className='footer-link' to="/help-center">Help Center</Link></li>
+        <li><Link className='footer-link' to="/gift-cards">Gift Cards</Link></li>
+        <li><Link className='footer-link'to="/media-center">Media Center</Link></li>
+        <li><Link className='footer-link' to="/investor-relations">Investor Relations</Link></li>
+        <li><Link className='footer-link' to="/jobs">Jobs</Link></li>
+        <li><Link  className='footer-link'to="/terms-of-use">Terms of Use</Link></li>
+        <li><Link className='footer-link'to="/privacy">Privacy</Link></li>
+        <li><Link className='footer-link' to="/legal-notices">Legal Notices</Link></li>
+        <li><Link className='footer-link' to="/cookie-preferences">Cookie Preferences</Link></li>
+        <li><Link className='footer-link'to="/corporate-information">Corporate Information</Link></li>
+        <li><Link className='footer-link' to="/contact-us">Contact Us</Link></li>
+      </ul>
+      <p className='copyright-text'>© 1997-2025 Netflix, Inc.</p>
     </div>
   )
 }
 
+  
 export default Footer
